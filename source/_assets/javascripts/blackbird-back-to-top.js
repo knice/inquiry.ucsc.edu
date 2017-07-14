e16a7fd1dae616346236f677245efd80901b79fb
Jason Chafin <jason@blackbirdconsult.com>
2017-07-14 15:40:48 -0700

    created include html for back-to-top button, included in all templates, edited primary index.html file to reflect current issue

--- source/_assets/javascripts/blackbird-back-to-top.js
+++ source/_assets/javascripts/blackbird-back-to-top.js
@@ -1,10 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- * @link https://premium.wpmudev.org/blog/back-to-top-button-wordpress/
- */
-
 jQuery(document).ready(function($){
     var offset = 100;
     var speed = 250;
